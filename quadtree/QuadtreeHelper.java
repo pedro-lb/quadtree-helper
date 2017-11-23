9e312a0ed012450fe52a22fb61eead00c7854231
Pedro Luis <pedro-1451@hotmail.com>
2017-11-23 00:50:13 -0200

    Add files via upload
    -- Added support for max depth
    
    -- Added partial support for color images

--- quadtree/QuadtreeHelper.java
+++ quadtree/QuadtreeHelper.java
@@ -2,6 +2,7 @@ package quadtree;
 
 import ij.ImagePlus;
 import ij.process.ByteProcessor;
+import ij.process.ColorProcessor;
 import ij.process.ImageConverter;
 import ij.process.ImageProcessor;
 import java.awt.image.BufferedImage;
@@ -13,12 +14,12 @@ import quadtree.models.ImageSection;
  */
 public class QuadtreeHelper {
 
-    public static ImagePlus CompressImage(ImagePlus image) {
-        ImageConverter conv = new ImageConverter(image);
-        conv.convertToGray8();
-        image.updateAndDraw();
-        
-        return GetImageFromQuadtree(image, GetQuadtree(image));
+    public static ImagePlus CompressImage(ImagePlus image, int maxDepth, boolean eightBitMode) {
+        if (eightBitMode) {
+            image = ConvertToGray8(image);
+        }
+
+        return GetImageFromQuadtree(image, GetQuadtree(image, maxDepth, eightBitMode), maxDepth, eightBitMode);
     }
 
     public static boolean IsBlank(ImagePlus image) {
@@ -37,50 +38,58 @@ public class QuadtreeHelper {
         }
     }
 
-    public static ImageSection GetQuadtree(ImagePlus image) {
+    public static ImageSection GetQuadtree(ImagePlus image, int maxDepth, boolean eightBitMode) {
         ImagePlus Result = image.duplicate();
+
+        if (!eightBitMode) {
+            Result = ConvertToGray8(Result);
+        }
 
         ByteProcessor bp = (ByteProcessor) Result.getProcessor();
         bp.autoThreshold();
 
-        ImageSection Quadtree = RecursiveSplit(new ImageSection(Result));
+        ImageSection Quadtree = RecursiveSplit(new ImageSection(Result), maxDepth, 0);
         return Quadtree;
     }
 
-    public static ImagePlus GetImageFromQuadtree(ImagePlus image, ImageSection Quadtree) {
-        ImagePlus Result = RecursiveClear(image, Quadtree);
+    public static ImagePlus GetImageFromQuadtree(ImagePlus image, ImageSection Quadtree, int maxDepth, boolean eightBitMode) {
+        ImagePlus Result = RecursiveClear(image, Quadtree, maxDepth, 0, eightBitMode);
         return Result;
     }
 
-    private static ImagePlus RecursiveClear(ImagePlus image, ImageSection Quadtree) {
+    private static ImagePlus RecursiveClear(ImagePlus image, ImageSection Quadtree, int maxDepth, int depth, boolean eightBitMode) {
+        if (maxDepth > 0 && depth >= maxDepth) {
+            return image;
+        }
+
         int Height = image.getHeight();
         int Width = image.getWidth();
+        depth++;
 
         if (Quadtree == null || Quadtree.Image == null || IsBlank(Quadtree.Image)) {
-            image = DrawByReference(image, null, 0, Width, 0, Height);
-
+            image = DrawByReference(image, null, 0, Width, 0, Height, eightBitMode);
         } else if (Height > 1 && Width > 1) {
             ImageSection ImageSection = Split(image);
 
             if (ImageSection != null) {
                 if (ImageSection.UpperLeftSection != null) {
-                    ImagePlus UpperLeftSection = RecursiveClear(ImageSection.UpperLeftSection.Image, Quadtree.UpperLeftSection);
-                    image = DrawByReference(image, UpperLeftSection, 0, Width / 2, 0, Height / 2);
+                    ImagePlus UpperLeftSection = RecursiveClear(ImageSection.UpperLeftSection.Image, Quadtree.UpperLeftSection, maxDepth, depth, eightBitMode);
+                    image = DrawByReference(image, UpperLeftSection, 0, Width / 2, 0, Height / 2, eightBitMode);
                 }
 
                 if (ImageSection.UpperRightSection != null) {
-                    ImagePlus UpperRightSection = RecursiveClear(ImageSection.UpperRightSection.Image, Quadtree.UpperRightSection);
-                    image = DrawByReference(image, UpperRightSection, Width / 2, Width, 0, Height / 2);
+                    ImagePlus UpperRightSection = RecursiveClear(ImageSection.UpperRightSection.Image, Quadtree.UpperRightSection, maxDepth, depth, eightBitMode);
+                    image = DrawByReference(image, UpperRightSection, Width / 2, Width, 0, Height / 2, eightBitMode);
                 }
 
                 if (ImageSection.LowerLeftSection != null) {
-                    ImagePlus LowerLeftSection = RecursiveClear(ImageSection.LowerLeftSection.Image, Quadtree.LowerLeftSection);
-                    image = DrawByReference(image, LowerLeftSection, 0, Width / 2, Height / 2, Height);
+                    ImagePlus LowerLeftSection = RecursiveClear(ImageSection.LowerLeftSection.Image, Quadtree.LowerLeftSection, maxDepth, depth, eightBitMode);
+                    image = DrawByReference(image, LowerLeftSection, 0, Width / 2, Height / 2, Height, eightBitMode);
                 }
 
                 if (ImageSection.LowerRightSection != null) {
-                    ImagePlus LowerRightSection = RecursiveClear(ImageSection.LowerRightSection.Image, Quadtree.LowerRightSection);
-                    image = DrawByReference(image, LowerRightSection, Width / 2, Width, Height / 2, Height);
+                    ImagePlus LowerRightSection = RecursiveClear(ImageSection.LowerRightSection.Image, Quadtree.LowerRightSection, maxDepth, depth, eightBitMode);
+                    image = DrawByReference(image, LowerRightSection, Width / 2, Width, Height / 2, Height, eightBitMode);
                 }
             }
         }
@@ -89,28 +98,47 @@ public class QuadtreeHelper {
         return image;
     }
 
-    private static ImagePlus DrawByReference(ImagePlus image, ImagePlus reference, int initialWidth, int finalWidth, int initialHeight, int finalHeight) {
+    private static ImagePlus DrawByReference(ImagePlus image, ImagePlus reference, int initialWidth, int finalWidth, int initialHeight, int finalHeight, boolean eightBitMode) {
         ImageProcessor ip = image.getProcessor();
         int X = 0, Y = 0;
+
+        if (!eightBitMode) {
+            ip = (ColorProcessor) ip;
+        }
 
         if (reference != null) {
             ImageProcessor ipReference = reference.getProcessor();
             for (int i = initialWidth; i < finalWidth; i++) {
                 for (int j = initialHeight; j < finalHeight; j++) {
-                    int Pixel = ipReference.getPixel(X, Y);
-                    if (Pixel == 255) {
-                        ip.putPixel(i, j, Pixel);
+                    if (eightBitMode) {
+                        int Pixel = ipReference.getPixel(X, Y);
+                        if (Pixel == 255) {
+                            ip.putPixel(i, j, new int[]{255, 255, 255});
+                        }
+                    } else {
+                        int[] ReferenceRGB = new int[]{0, 0, 0};
+                        ipReference.getPixel(i, j, ReferenceRGB);
+
+                        int[] RGB = new int[]{0, 0, 0};
+                        ip.getPixel(i, j, RGB);
+
+                        // TODO: Draw the rectangle as an average color of itself.
+                        ip.putPixel(i, j, new int[]{(ReferenceRGB[0] + RGB[0]) / 2, (ReferenceRGB[1] + RGB[1]) / 2, (ReferenceRGB[2] + RGB[2]) / 2});
                     }
+
                     Y++;
                 }
                 Y = 0;
                 X++;
-
             }
         } else {
             for (int i = initialWidth; i < finalWidth; i++) {
                 for (int j = initialHeight; j < finalHeight; j++) {
-                    ip.putPixel(i, j, 255);
+                    if (eightBitMode) {
+                        ip.putPixel(i, j, 255);
+                    } else {
+                        ip.putPixel(i, j, new int[]{255, 255, 255});
+                    }
                 }
             }
         }
@@ -119,15 +147,19 @@ public class QuadtreeHelper {
         return image;
     }
 
-    private static ImageSection RecursiveSplit(ImageSection image) {
+    private static ImageSection RecursiveSplit(ImageSection image, int maxDepth, int depth) {
+        if (maxDepth > 0 && depth >= maxDepth) {
+            return image;
+        }
+
         if (image != null) {
-
             image = Split(image.Image);
-            image.UpperLeftSection = RecursiveSplit(image.UpperLeftSection);
-            image.UpperRightSection = RecursiveSplit(image.UpperRightSection);
-            image.LowerLeftSection = RecursiveSplit(image.LowerLeftSection);
-            image.LowerRightSection = RecursiveSplit(image.LowerRightSection);
+            depth++;
 
+            image.UpperLeftSection = RecursiveSplit(image.UpperLeftSection, maxDepth, depth);
+            image.UpperRightSection = RecursiveSplit(image.UpperRightSection, maxDepth, depth);
+            image.LowerLeftSection = RecursiveSplit(image.LowerLeftSection, maxDepth, depth);
+            image.LowerRightSection = RecursiveSplit(image.LowerRightSection, maxDepth, depth);
         }
 
         return image;
@@ -176,5 +208,12 @@ public class QuadtreeHelper {
 
         BufferedImage croppedImage = ipCropped.getBufferedImage();
         return new ImagePlus("Result", croppedImage);
+    }
+
+    private static ImagePlus ConvertToGray8(ImagePlus image) {
+        ImageConverter converter = new ImageConverter(image);
+        converter.convertToGray8();
+        image.updateAndDraw();
+        return image;
     }
 }
--- quadtree/TestUnit.java
+++ quadtree/TestUnit.java
@@ -1,7 +1,6 @@
 package quadtree;
 
 import ij.ImagePlus;
-import ij.process.ImageConverter;
 
 /**
  *
@@ -10,13 +9,54 @@ import ij.process.ImageConverter;
 public class TestUnit {
 
     public static void main(String[] args) {
-        ImagePlus img = ij.IJ.openImage();
-        ImagePlus compressed = QuadtreeHelper.CompressImage(img.duplicate());
+        //Test_Lena();        
 
-        img.setTitle("Original");
-        compressed.setTitle("Compressed");
+        //Test_Feynman();
+        //Test_PickImage(false);
+    }
 
-        img.show();
-        compressed.show();
+    public static void Test_PickImage(boolean eightBitMode) {
+        ImagePlus image = ij.IJ.openImage();
+        if (image != null) {
+            ShowQuadtreeResult(image, 6, eightBitMode);
+            ShowQuadtreeResult(image, 7, eightBitMode);
+            ShowQuadtreeResult(image, 8, eightBitMode);
+            ShowQuadtreeResult(image, 9, eightBitMode);
+            ShowQuadtreeResult(image, 0, eightBitMode);
+            image.setTitle("Original");
+            image.show();
+        }
+    }
+
+    public static void Test_Feynman() {
+        ImagePlus image = new ImagePlus("src/quadtree/images/feynman.jpg");
+        if (image != null) {
+            ShowQuadtreeResult(image, 6, false);
+            ShowQuadtreeResult(image, 7, false);
+            ShowQuadtreeResult(image, 8, false);
+            ShowQuadtreeResult(image, 9, false);
+            ShowQuadtreeResult(image, 0, false);
+            image.setTitle("Original");
+            image.show();
+        }
+    }
+
+    public static void Test_Lena() {
+        ImagePlus image = new ImagePlus("src/quadtree/images/lena_gray.bmp");
+        if (image != null) {
+            ShowQuadtreeResult(image, 6, true);
+            ShowQuadtreeResult(image, 7, true);
+            ShowQuadtreeResult(image, 8, true);
+            ShowQuadtreeResult(image, 9, true);
+            ShowQuadtreeResult(image, 0, true);
+            image.setTitle("Original");
+            image.show();
+        }
+    }
+
+    public static void ShowQuadtreeResult(ImagePlus image, int maxDepth, boolean eightBitMode) {
+        ImagePlus quadtreeResult = QuadtreeHelper.CompressImage(image.duplicate(), maxDepth, eightBitMode);
+        quadtreeResult.setTitle("Quadtree Result - Depth: " + (maxDepth > 0 ? maxDepth : "Max") + (eightBitMode ? " (8-bit)" : ""));
+        quadtreeResult.show();
     }
 }
